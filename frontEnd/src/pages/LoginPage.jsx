afbfbc48f45b42555b775a31713cc28af748bb0b
Ireme-Nyawe <akimana.inono@gmail.com>
2025-01-19 15:47:07 +0200

    add forgot message

--- frontEnd/src/pages/LoginPage.jsx
+++ frontEnd/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useToast } from "../components/toasts/ToastManager"
+import { useToast } from "../components/toasts/ToastManager";
 import { login } from "../slices/userSlice";
 
 export default function LoginPage() {
@@ -10,6 +10,8 @@ export default function LoginPage() {
   const [errors, setErrors] = useState({});
   const { addToast } = useToast();
   const navigate = useNavigate();
+  const [isForgotPasswordVisible, setIsForgotPasswordVisible] = useState(false);
+
   const validateInputs = () => {
     const errors = {};
     if (!email) {
@@ -43,8 +45,8 @@ export default function LoginPage() {
       if (response.status === 200) {
         setIsLoading(false);
         localStorage.clear();
-        localStorage.setItem("userIdToLogin", response.session.userId)
-        localStorage.setItem("token", response.session.userId)
+        localStorage.setItem("userIdToLogin", response.session.userId);
+        localStorage.setItem("token", response.session.userId);
         addToast("success", response.message, 3000);
         navigate("/dashboard");
       } else {
@@ -56,79 +58,105 @@ export default function LoginPage() {
       addToast("error", error.message || "An unexpected error occurred.", 3000);
     }
   };
-    return (
-      <div className="bg-primary min-h-screen flex items-center justify-center">
-        <div className="bg-secondary p-8 rounded-lg shadow-lg text-center w-[500px]"> {/* Increased width */}
-          <h1 className="text-accent text-3xl font-bold mb-4">Wallet App</h1>
-          <p className="text-dark mb-6">Welcome! Please log in to continue.</p>
-          
-          <form className="space-y-4" onSubmit={handleLogin}>
-            <div>
-              <input
-                type="text"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full p-3 border border-neutral rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
-              />
-              {errors.email && (
-                  <p className="text-red-500 text-sm mt-2">{errors.email}</p>
-                )}
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full p-3 border border-neutral rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
-              />
-              {errors.password && (
-                  <p className="text-red-500 text-sm mt-2">{errors.password}</p>
-                )}
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-accent text-primary py-3 rounded-md font-semibold hover:bg-highlight transition"
-            >
-               {isLoading ? (
-                  <span className="flex items-center justify-center">
-                    <svg
-                      className="animate-spin h-5 w-5 mr-2 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                      ></path>
-                    </svg>
-                    Loading...
-                  </span>
-                ) : (
-                  "Login"
-                )}
-            </button>
-          </form>
-  
-          <div className="mt-4">
-            <a
-              href="#"
-              className="text-highlight hover:underline text-sm"
-            >
-              Forgot Password?
-            </a>
+
+  const toggleForgotPassword = () => {
+    setIsForgotPasswordVisible(!isForgotPasswordVisible);
+  };
+
+  return (
+    <div className="bg-primary min-h-screen flex flex-col items-center justify-center">
+      <div className="bg-secondary p-8 rounded-lg shadow-lg text-center w-[500px]">
+        <h1 className="text-accent text-3xl font-bold mb-4">Wallet App</h1>
+        <p className="text-dark mb-6">Welcome! Please log in to continue.</p>
+
+        <form className="space-y-4" onSubmit={handleLogin}>
+          <div>
+            <input
+              type="text"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-3 border border-neutral rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-2">{errors.email}</p>
+            )}
           </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-3 border border-neutral rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-2">{errors.password}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-accent text-primary py-3 rounded-md font-semibold hover:bg-highlight transition"
+          >
+            {isLoading ? (
+              <span className="flex items-center justify-center">
+                <svg
+                  className="animate-spin h-5 w-5 mr-2 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                  ></path>
+                </svg>
+                Loading...
+              </span>
+            ) : (
+              "Login"
+            )}
+          </button>
+        </form>
+
+        <div className="mt-4">
+          <button
+            className="text-highlight hover:underline text-sm"
+            onClick={toggleForgotPassword}
+          >
+            Forgot Password?
+          </button>
         </div>
       </div>
-    );
-  }
-  
+
+      {isForgotPasswordVisible && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white p-6 rounded-md shadow-lg max-w-sm">
+            <h2 className="text-lg font-semibold mb-2">Forgot Password</h2>
+            <p className="text-sm text-gray-700 mb-4">
+              This option is not available at the moment as the app is being used by one user. 
+              Please refer to the app documentation to understand how to use it. 
+              For special cases, contact the product owner directly.
+            </p>
+            <button
+              onClick={toggleForgotPassword}
+              className="bg-highlight text-white px-4 py-2 rounded-md hover:bg-red-600 transition"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+      <footer className="text-gray-600 text-sm mt-6">
+        &copy; 2025 AKIMANA. All rights reserved. patenered COA with DCCS
+      </footer>
+    </div>
+  );
+}
